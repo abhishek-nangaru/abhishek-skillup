d04915ebbeb9ef63450b0b8b3b5155b10dbac5c1
abhishek-nangaru <abhi13.ns@gmail.com>
2025-05-19 13:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function(request, response) {
-  response.send("hello from express js....")
+  response.send("hello from express js...., This is my jenkins project integrating with docker in skillup")
 })
 
 app.get("/date", function(request, response) {
   response.send("Date is:.." + new Date().toLocaleString())
 })
 
-app.listen(9595, function(err) {
+app.listen(9696, function(err) {
   if (err) throw err
-  console.log("Server started on port 9595, to shut down hit ctrl+c")
+  console.log("Server started on port 9696, to shut down hit ctrl+c")
 })
