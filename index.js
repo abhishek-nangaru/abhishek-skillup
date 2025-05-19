6f52c2c8707ff99023c8188073a19518ae2b07dc
abhishek-nangaru <abhi13.ns@gmail.com>
2025-05-19 12:43:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var express=require('express')
-var bodyParser=require('body-parser')
-var mysql=require('mysql')
-var app=express()
-app.use(bodyParser())
+var express = require('express')
+var bodyParser = require('body-parser')
+var mysql = require('mysql')
+var app = express()
 
-app.get("/",function(request,response){
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function(request, response) {
   response.send("hello from express js....")
-});
+})
 
+app.get("/date", function(request, response) {
+  response.send("Date is:.." + new Date().toLocaleString())
+})
 
-app.get("/date",function(request,response){
-  response.send("Date is:.."+new Date().toLocaleString())
-});
-
-app.listen(9595,function(err){
+app.listen(9595, function(err) {
   if (err) throw err
   console.log("Server started on port 9595, to shut down hit ctrl+c")
 })
